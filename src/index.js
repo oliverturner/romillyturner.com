330bec7af19398ae8912dfdd13fbf6f2e2a7b49f
Oliver Turner <oliver.turner@codedsignal.co.uk>
2016-10-10 01:15:27 +0100

    CSS tweaks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import 'sanitize.css/sanitize.css'
+
 import './index.css';
+import App from './app';
 
 ReactDOM.render(
   <App />,
